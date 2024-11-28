a277f3eb00cfd9cae4be1c3e31d4717c1e07e0c4
Kaxramonovravshan <r17ravshan@gmail.com>
2024-11-28 11:01:35 +0500

    first commit

---
+++ app/direktor/page.tsx
@@ -0,0 +1,146 @@
+import React from "react";
+
+function Direktor() {
+  return (
+    <div className="w-full">
+      <div className="max-w-7xl mx-auto p-3">
+        <h1 className="text-center text-blue-700 mb-2 text-2xl font-bold">
+          Assalomu alaykum!
+        </h1>
+        <h3 className="text-center text-blue-700 mb-5 text-2xl font-bold">
+          Olmaliq shahar 8-sonli bolalar musiqa va san’at maktabining rasmiy
+          saytiga xush kelibsiz!
+        </h3>
+        <div className="flex flex-col md:flex-row align-center gap-2">
+          <img
+            className="md:w-1/5  w-full"
+            src="https://olmaliqbmsm.uz/img/Direktor.jpg"
+            alt=""
+          />
+          <p className="my-2 ">
+            Bizning maktabimiz 1955-yil 18-mayda tashkil etilgan. Bu paytda
+            maktabda uchta o‘qituvchi va ikkita musiqa asbobi-fortepiano va
+            bayan mavjud bo‘lgan. Maktabning birinchi direktori Stepanov Nikolay
+            Osipovich bo‘lib, u uzluksiz 48 yil davomida ushbu maktabga
+            rahbarlik qilib keldi. 2003-yildan 2016-yilga qadar O‘zbekiston
+            Respublikasi Xalq Ta’limi a’lochisi Kuligina Lidiya Nikolayevna
+            maktab direktori lavozimida faoliyat ko‘rsatib, maktabning ravnaqi
+            yo‘lida jonbozlik ko‘rsatdi. 2016-yildan 2023-yilga qadar maktabga
+            Furman Mixail Evaldovich direktorlik qildi. Yillar davomida maktabda
+            o‘qituvchilar hamda o‘quvchilar soni salmoqli ravishda oshdi.
+            1971-yil musiqa maktab uchun aloxida bino qurildi. 1980-yil yakuniga
+            ko‘ra musiqa maktabi Respublika miqyosida eng namunali maktab
+            sifatida e’tirof etilgan. 1997-yilda musiqa maktabida tasviriy
+            san’at va xoreografiya sinflari, 2007-yil estrada cholg‘u
+            ijrochiligi yo‘nalishi, 2011-yil estrada ijrochiligi yo‘nalishi,
+            2013-yil teatr, zardo‘zlik, 2014-yil dutor hamda an’anaviy
+            xonandalik sinflari, 2017-yil chang, g’ijjak-kontrabas, 2021-yil
+            bariton va trombone sinflari tashkil etildi. 2000-yil maktab
+            “Olmaliq kon-metallurgiya kombinati” AJ tomonidan kapital
+            ta’mirlandi. 2008-yil maktab 8-sonli Bolalar musiqa va san’at
+            maktabi deb nomlandi. Ta’lim-tarbiya jarayonini pedagogik va uslubiy
+            jihatdan samarali tashkil etish maqsadida bolalarni har tomonlama
+            rivojlantirishga yo‘naltirilgan uslubiy qo‘llanmalardan mohirona
+            foydalanib kelinmoqda. Birinchi Prezidentimizning 2008-yil
+            8-iyuldagi «Bolalar musiqa va san’at maktablarining moddiy-texnik
+            bazasini mustahkamlash va ularning faoliyatini yanada yaxshilash
+            bo‘yicha 2009-2014 yillarga mo‘ljallangan Davlat dasturi
+            to‘g‘risida»gi 910-qaroriga muvofiq bizning maktab 2012-yilda to‘liq
+            qayta ta’mirlanib moddiy-texnik bazasi yanada yaxshilandi. Maktab
+            zamonaviy musiqa cholg‘ulari, ovoz kuchaytirgich, mebel va uskunalar
+            bilan jihozlandi, bosqichma-bosqich ravishda zarur o‘quv-uslubiy
+            qo‘llanmalar, notalar, SD disk va ko‘rgazmali materiallar bilan
+            ta’minlanib kelinmoqda. Maktab kutuxonasida 13500 ta kitob mavjud.
+            Shuningdek, kutubxona qoshida elektron kutubxona tashkil qilingan
+            bo‘lib uninig bazasi 5000 ga yaqin elektron kitobni tashkil etadi.
+          </p>
+        </div>
+        <p className="my-2 ">
+          Shuni alohida ta’kidlash joizki, bolalar musiqa va san’at maktabida
+          yaratilgan zamonaviy shart-sharoitlar natijasida o‘quvchilar qamrovi
+          so'nggi yillarda keskin ortdi. Hozirgi kunda maktabimizda 615 o‘quvchi
+          musiqa va san’atning 13 ta yo‘nalishi bo‘yicha ta’lim olmoqda. Ularga
+          57 ta o‘z ishini puxta biladigan pedagoglar ta’lim sirlarini o‘rgatib
+          kelishmoqda.
+        </p>
+        <p className="my-2 ">
+          Maktabda bolalarning ma’lum bir cholg‘u asobini chalish texnikasini
+          egallash bilan bir qatorda, jahon mumtoz musiqasini, dunyoga tanilgan
+          buyuk bastakorlar va rassomlarning ijodini chuqur o‘rganish,
+          o‘quvchilarning musiqiy va umumiy madaniyatini oshirish, bolalarda
+          butun umri davomida xamroh bo‘ladigan musiqa va san’atning boshqa
+          turlariga qiziqishlarini kuchaytirish va muhabbatni har tomonlama
+          singdirish ishlari olib borilmoqda.
+        </p>
+        <p className="my-2 ">
+          Bolalar musiqa va san’at maktabi o‘quvchilarining madaniy saviyasini
+          yuksaltirish, milliy va jahon musiqalari, tasviriy san’atning yuksak
+          namunalaridan keng baxramand bo‘lishlari uchun zarur sharoitlar
+          yaratilgan. O‘quv tarbiya jarayoni Bolalar musiqa va san’at
+          maktablariga qo‘yilgan davlat talablari asosida tashkil etilgan,
+          Madaniyat va turizm boshqarmasi tomonidan taqdim qilingan va
+          o‘rnatilgan tartibda tasdiqlangan o‘quv dasturlari mavjud va ularga
+          xozirgi zamon talablariga mutanosib keladi.
+        </p>
+        <p className="my-2 ">
+          Maktabimiz o‘quvchilari nafaqat mamlakatimizda, balki jahon miqyosida
+          o‘tkazilayotgan tanlovlarda va festivallarda, viloyat miqyosida
+          tashkil etiladigan barcha bayram tadbirlarida ishtirok etib kelishadi.
+        </p>
+        <p className="my-2 ">
+          2020-yil LE FESTIVAL INTERNATIONAL PERFEKT” xalqaro ko‘rik-tanlovida
+          tasviriy san’at sinfi o‘quvchisi Tilavova Diyora Dilmurod qizi
+          2-o‘rinni;
+        </p>
+        <p className="my-2 ">
+          2021-yil “PEACE AND FRIENDSHIP» xalqaro ko‘rik-tanlovida rubob sinfi
+          o‘quvchisi Abdumutalov Bexruz A’zam o‘g‘li 1-o‘rinni;
+        </p>
+        <p className="my-2 ">
+          2021-yil “LATVIA FLAVOURS” xalqaro ko‘rik-tanlovida rubob sinfi
+          o‘quvchisi Toirov Firdavs Samandar o‘g‘li 1-o‘rinni;
+        </p>
+        <p className="my-2 ">
+          2021-yil I.S. Bax ijodiga bag‘ishlab o‘tkazilgan Xalqaro cholg‘u
+          ijrochiligi ko‘rik-tanlovida fortepiano sinfi o‘quvchisi Ilyasov Usein
+          Rustemovich 2-o‘rinni;
+        </p>
+        <p className="my-2 ">
+          2022-yil Samarqand shahrida o‘tkazilgan “San’at yo‘nalishi” Respublika
+          ko‘rik-tanlovida teatr sinfi jamoasi 3-o‘rinni egalladilar.
+        </p>
+        <p className="my-2 ">
+          Yoshlarga ta’lim berishda ko‘rsatgan jonbozligi va fidokorona mehnati
+          uchun 2019-yil Furman Mixail Evaldovich “Shuxrat” medali bilan,
+          2022-yil violonchel sinfi o‘qituvchisi Solomonova Svetlana Andreyevna
+          “Sog‘lom avlod” ko‘krak nishoni bilan taqdirlandi.
+        </p>
+
+        <h4 className="text-center font-bold md:text-lg text-base text-blue-700">8-sonli bolalar musiqa va san’at maktabi direktori</h4>
+        <h1 className="text-center my-4 font-bold md:text-3xl text-lg text-blue-700">Niyozov Ravshan Ismoilovich</h1>
+        <div className="flex justify-between items-center">
+            <div className="w-1/4">
+                <img className="w-full" src="https://olmaliqbmsm.uz/img/xodim_ish_yurituvchi2.jpg" alt="" />
+                <p className="text-center">Ish yurituvchi</p>
+                <p className="text-center font-bold">Urmanova</p>
+                <p className="text-center">Aziza Nematilla qizi</p>
+            </div>
+            <div className="w-1/4">
+                <img src="https://olmaliqbmsm.uz/img/xodim_kutubxonachi.jpg" alt="" />
+                <p className="text-center">Kutubxonachi</p>
+                <p className="text-center font-bold">Urunboyeva</p>
+                <p className="text-center">Xusnora Sobirovna</p>
+            </div>
+            <div className="w-1/4">
+                <img src="https://olmaliqbmsm.uz/img/xodim_liboschi.jpg" alt="" />
+                <p className="text-center">Ish yurituvchi</p>
+                <p className="text-center font-bold">Urmanova</p>
+                <p className="text-center">Aziza Nematilla qizi</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Direktor;
---
+++ app/page.tsx
@@ -0,0 +1,207 @@
+import Image from "next/image";
+
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="w-full">
+        <div className="max-w-7xl mx-auto flex gap-3 p-2 items-center md:mb-20 mb-10">
+          <div className="w-1/5">
+            <img
+              className="w-full rounded-lg"
+              src="https://olmaliqbmsm.uz/img/T_9_may_2.jpg"
+              alt=""
+            />
+          </div>
+          <div className="w-1/5">
+            <img
+              className="w-full rounded-lg"
+              src="https://olmaliqbmsm.uz/img/T_taqdimot_11.jpg"
+              alt=""
+            />
+          </div>
+          <div className="w-1/5">
+            <img
+              className="w-full rounded-lg"
+              src="https://olmaliqbmsm.uz/img/T_8_mart_1.jpg"
+              alt=""
+            />
+          </div>
+          <div className="w-1/5">
+            <img
+              className="w-full rounded-lg"
+              src="https://olmaliqbmsm.uz/img/T_2HK_5.jpg"
+              alt=""
+            />
+          </div>
+          <div className="w-1/5">
+            <img
+              className="w-full rounded-lg"
+              src="https://olmaliqbmsm.uz/img/T_2HK_6.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="max-w-7xl mx-auto flex flex-col md:flex-row align-center justify-between">
+          <div className="md:w-1/4 w-full p-2">
+            <ul className="text-center flex flex-wrap">
+              <Link
+                href={"/direktor"}
+                className="font-medium block mb-1 rounded-lg hover:cursor-pointer hover:text-white hover:bg-blue-700 p-2"
+              >
+                BMSM direktorining taqdimoti
+              </Link>
+              <Link
+                href={""}
+                className="font-medium block mb-1 rounded-lg hover:cursor-pointer hover:text-white hover:bg-blue-700 p-2"
+              >
+                Maktab ko‘rsatkichlari
+              </Link>
+              <Link
+                href={""}
+                className="font-medium block mb-1 rounded-lg hover:cursor-pointer hover:text-white hover:bg-blue-700 p-2"
+              >
+                O‘quvchilar uchun
+              </Link>
+              <Link
+                href={""}
+                className="font-medium block mb-1 rounded-lg hover:cursor-pointer hover:text-white hover:bg-blue-700 p-2"
+              >
+                O‘quvchilar xavfsizligi
+              </Link>
+              <Link
+                href={""}
+                className="font-medium block mb-1 rounded-lg hover:cursor-pointer hover:text-white hover:bg-blue-700 p-2"
+              >
+                Mehnat muhofazasi
+              </Link>
+              <Link
+                href={""}
+                className="font-medium block mb-1 rounded-lg hover:cursor-pointer hover:text-white hover:bg-blue-700 p-2"
+              >
+                Ota-onalar, Sizlar uchun!
+              </Link>
+              <Link
+                href={""}
+                className="font-medium block mb-1 rounded-lg hover:cursor-pointer hover:text-white hover:bg-blue-700 p-2"
+              >
+                Bizning yutuqlar
+              </Link>
+              <Link
+                href={""}
+                className="font-medium block mb-1 rounded-lg hover:cursor-pointer hover:text-white hover:bg-blue-700 p-2"
+              >
+                Hamkorlik memorandumlari
+              </Link>
+              <Link
+                href={""}
+                className="font-medium block mb-1 rounded-lg hover:cursor-pointer hover:text-white hover:bg-blue-700 p-2"
+              >
+                Davlat xaridlari
+              </Link>
+              <Link
+                href={""}
+                className="font-medium block mb-1 rounded-lg hover:cursor-pointer hover:text-white hover:bg-blue-700 p-2"
+              >
+                Bo‘sh ish o‘rinlari
+              </Link>
+              <Link
+                href={""}
+                className="font-medium block mb-1 rounded-lg hover:cursor-pointer hover:text-white hover:bg-blue-700 p-2"
+              >
+                Ma’naviyat va ma’rifat
+              </Link>
+              <Link
+                href={""}
+                className="font-medium block mb-1 rounded-lg hover:cursor-pointer hover:text-white hover:bg-blue-700 p-2"
+              >
+                Virtual qabulxona
+              </Link>
+            </ul>
+          </div>
+          <div className="md:w-3/4 w-full p-2">
+            <div className="flex flex-col md:flex-row align-center  gap-3 mb-5">
+              <img
+                className="md:w-56 w-full"
+                src="https://olmaliqbmsm.uz/img/Tadbirlar/56_1.jpg"
+                alt=""
+              />
+              <div>
+                <h1 className="text-center text-blue-700 font-bold md:text-3xl text-xl mb-2">
+                  Zarkentda uzum sayli bo‘lib o‘tdi
+                </h1>
+                <p className="font-normal mb-4 md:text-start text-center">
+                  Joriy yilning 23-25-avgust kunlari Parkent tumaning Zarkent
+                  qishlog‘ida uzum sayli festivali bo‘lib o‘tdi. Festivalni
+                  o‘tkazishdan ko‘zlangan asosiy maqsad, nafaqat yurtimiz,
+                  balki, xorijga turizm salohiyatini keng namoyish etish, milliy
+                  mahsulotimiz brendini jahon bozorida munosib o‘ringa ega
+                  bo‘lishini ta’minlash, qadimiy uzumchilik va vinochilik
+                  tajribasi hamda bog‘dorchilik mahsulotlarining noyob tur va
+                  navlarini keng ommalashtirish bo‘ldi.
+                </p>
+                <div className="flex gap-7 items-center md:justify-start justify-center ">
+                  <p>25.08.2024</p>
+                  <button className="hover:underline">Batafsil</button>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col md:flex-row align-center  gap-3 mb-5">
+              <img
+                className="md:w-56 w-full"
+                src="https://olmaliqbmsm.uz/img/Tadbirlar/56_1.jpg"
+                alt=""
+              />
+              <div>
+                <h1 className="text-center text-blue-700 font-bold md:text-3xl text-xl mb-2">
+                  Zarkentda uzum sayli bo‘lib o‘tdi
+                </h1>
+                <p className="font-normal mb-4 md:text-start text-center">
+                  Joriy yilning 23-25-avgust kunlari Parkent tumaning Zarkent
+                  qishlog‘ida uzum sayli festivali bo‘lib o‘tdi. Festivalni
+                  o‘tkazishdan ko‘zlangan asosiy maqsad, nafaqat yurtimiz,
+                  balki, xorijga turizm salohiyatini keng namoyish etish, milliy
+                  mahsulotimiz brendini jahon bozorida munosib o‘ringa ega
+                  bo‘lishini ta’minlash, qadimiy uzumchilik va vinochilik
+                  tajribasi hamda bog‘dorchilik mahsulotlarining noyob tur va
+                  navlarini keng ommalashtirish bo‘ldi.
+                </p>
+                <div className="flex gap-7 items-center md:justify-start justify-center ">
+                  <p>25.08.2024</p>
+                  <button className="hover:underline">Batafsil</button>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col md:flex-row align-center  gap-3 mb-5">
+              <img
+                className="md:w-56 w-full"
+                src="https://olmaliqbmsm.uz/img/Tadbirlar/56_1.jpg"
+                alt=""
+              />
+              <div>
+                <h1 className="text-center text-blue-700 font-bold md:text-3xl text-xl mb-2">
+                  Zarkentda uzum sayli bo‘lib o‘tdi
+                </h1>
+                <p className="font-normal mb-4 md:text-start text-center">
+                  Joriy yilning 23-25-avgust kunlari Parkent tumaning Zarkent
+                  qishlog‘ida uzum sayli festivali bo‘lib o‘tdi. Festivalni
+                  o‘tkazishdan ko‘zlangan asosiy maqsad, nafaqat yurtimiz,
+                  balki, xorijga turizm salohiyatini keng namoyish etish, milliy
+                  mahsulotimiz brendini jahon bozorida munosib o‘ringa ega
+                  bo‘lishini ta’minlash, qadimiy uzumchilik va vinochilik
+                  tajribasi hamda bog‘dorchilik mahsulotlarining noyob tur va
+                  navlarini keng ommalashtirish bo‘ldi.
+                </p>
+                <div className="flex gap-7 items-center md:justify-start justify-center ">
+                  <p>25.08.2024</p>
+                  <button className="hover:underline">Batafsil</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
